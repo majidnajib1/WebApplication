0abe2636e1ceca1f10cbce9f82cbfef7092fcaed
m-najib <m-najib@BF39Z4J>
2018-05-23 11:30:47 +0100

    updated

--- WebApplication1/Test2.cs
+++ WebApplication1/Test2.cs
@@ -15,9 +15,9 @@ namespace WebApplication1
         public string Subject { get; set; }
 
         public string Type { get; set; }
-      
-       public string Description { get; set; }
 
+        public string Description { get; set; }
+    }
    
      
 }
